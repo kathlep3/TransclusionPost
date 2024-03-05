29d210d85b9fdecf789c5121cead1372b2f12010
Kathleen Pham <kathlep3@uci.edu>
2024-03-05 02:46:47 +0000

    Start

---
+++ OpenWeather.py
@@ -0,0 +1,11 @@
+# openweather.py
+
+# Starter code for assignment 4 in ICS 32
+# Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Kathleen Pham
+# kathlep3@uci.edu
+# 79281883
+
---
+++ a4.py
@@ -0,0 +1,10 @@
+# a4.py
+
+# Starter code for assignment 4 in ICS 32
+# Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Kathleen Pham
+# kathlep3@uci.edu
+# 79281883
